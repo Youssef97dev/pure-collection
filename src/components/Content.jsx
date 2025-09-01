3085f373f6c828794eea77e57caf2ddd31f4bb76
Youssef97dev <youssefmoussafi97@gmail.com>
2025-09-01 16:44:55 +0100

    sidebar

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -5,22 +5,54 @@ const Content = () => {
   return (
     <section className="grid grid-cols-[180px_1fr_1fr_1fr] gap-4 pt-6 h-screen">
       {/* Sidebar */}
-      <aside className=" text-black p-4">
-        <h2 className="text-lg font-bold mb-2">Sidebar</h2>
-        <ul className="space-y-2">
+      <aside className=" text-black p-4 flex flex-col justify-between items-start">
+        <ul className="space-y-5">
           <li>
-            <a href="#" className="block hover:underline">
-              Link 1
+            <a
+              href="#"
+              className="text-[21px] leading-[20px] mb-2 block hover:underline "
+            >
+              Pure Collection
             </a>
           </li>
           <li>
-            <a href="#" className="block hover:underline">
-              Link 2
+            <a
+              href="#"
+              className="text-[21px] leading-[20px] mb-2 block hover:underline "
+            >
+              Pure Fit
             </a>
           </li>
           <li>
-            <a href="#" className="block hover:underline">
-              Link 3
+            <a
+              href="#"
+              className="text-[21px] leading-[20px] mb-2 block hover:underline "
+            >
+              Event Calendar
+            </a>
+          </li>
+        </ul>
+        <ul className="space-y-5">
+          <li>
+            <a
+              href="#"
+              className="text-[21px] leading-[20px] mb-2 block hover:underline "
+            >
+              Instagram
+            </a>
+          </li>
+          <li className="flex justify-start items-start gap-4">
+            <a
+              href="#"
+              className="text-[21px] leading-[20px] mb-2 block hover:underline "
+            >
+              EN
+            </a>
+            <a
+              href="#"
+              className="text-[21px] leading-[20px] mb-2 block hover:underline "
+            >
+              FR
             </a>
           </li>
         </ul>
