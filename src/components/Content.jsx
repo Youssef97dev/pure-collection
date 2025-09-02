922b7a601ac3992ad002f94d11a56506d6962ccd
Youssef97dev <youssefmoussafi97@gmail.com>
2025-09-02 14:12:28 +0100

    sidebar desktop

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import Riad from "./Riad";
 import Farm from "./Farm";
 import Boho from "./Boho";
 
 const Content = () => {
   return (
-    <section className="grid grid-cols-[180px_1fr_1fr_1fr] gap-4 pt-6 h-screen">
+    <section className="grid grid-cols-[210px_1fr_1fr_1fr] gap-4 pt-6 h-screen">
       {/* Sidebar */}
-      <aside className=" text-black p-4 flex flex-col justify-between items-start">
-        <ul className="space-y-5">
+      <aside className=" text-black p-1 flex flex-col justify-between items-start">
+        <ul className="space-y-5 uppercase">
           <li>
             <a
               href="#"
@@ -35,28 +36,20 @@ const Content = () => {
             </a>
           </li>
         </ul>
-        <ul className="space-y-5">
-          <li>
-            <a
-              href="#"
-              className="text-[21px] leading-[20px] mb-2 block hover:underline "
-            >
-              Instagram
-            </a>
+        <ul className="space-y-3 ">
+          <li className="text-[21px] leading-[20px]">
+            <Link href="#">{`Instagram`}</Link>
           </li>
-          <li className="flex justify-start items-start gap-4">
-            <a
-              href="#"
-              className="text-[21px] leading-[20px] mb-2 block hover:underline "
-            >
-              EN
-            </a>
-            <a
-              href="#"
-              className="text-[21px] leading-[20px] mb-2 block hover:underline "
-            >
-              FR
-            </a>
+          <li className="flex justify-start items-start gap-4 text-[21px] leading-[20px]">
+            <Link href="#">EN</Link>
+            <Link href="#">FR</Link>
+          </li>
+          <li className="flex flex-col justify-start items-start gap-0 lowercase text-[12px]">
+            <Link href="#">{`reservations@purehousemarrakech.com`}</Link>
+            <Link href="#">{`+212 6 82 22 52 35`}</Link>
+          </li>
+          <li className="lowercase text-[12px]">
+            <Link href="#">{`16 Derb Abou El Fdail, Marrakech 40000 Morocco`}</Link>
           </li>
         </ul>
       </aside>
