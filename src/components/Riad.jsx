49ce770961ebe630afd9933ae2d88933cd024802
Youssef97dev <youssefmoussafi97@gmail.com>
2025-09-04 11:25:45 +0100

    add text in riad 3

--- src/components/Riad.jsx
+++ src/components/Riad.jsx
@@ -18,7 +18,7 @@ const Riad = () => {
         </p>
       </div>
       <Image
-        src="/riad-1.webp"
+        src="/riad-2.webp"
         alt="pure house riad"
         width={1000}
         height={1000}
@@ -31,7 +31,38 @@ const Riad = () => {
         height={1000}
         className="object-cover w-full"
       />
-      <p className="text-[13px] leading-[15px] text-left">{t("riad.text_3")}</p>
+      <div className="flex flex-col justify-start items-center gap-0">
+        <div className="w-full flex justify-between items-center gap-10">
+          <h1 className="text-[18px] leading-[20px] whitespace-nowrap">{`Pure House Riad`}</h1>
+          <p className="text-[13px] leading-[15px] text-left">
+            {t("riad.text_3")}
+          </p>
+        </div>
+        <p className="text-[13px] leading-[15px] text-left">
+          {t("riad.text_4")}
+        </p>
+      </div>
+      <Image
+        src="/riad-1.webp"
+        alt="pure house riad"
+        width={1000}
+        height={1000}
+        className="object-cover w-full"
+      />
+      <Image
+        src="/riad-3.webp"
+        alt="pure house riad"
+        width={1000}
+        height={1000}
+        className="object-cover w-full"
+      />
+      <Image
+        src="/riad-4.webp"
+        alt="pure house riad"
+        width={1000}
+        height={1000}
+        className="object-cover w-full"
+      />
     </div>
   );
 };
