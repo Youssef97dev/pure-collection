0ef77af710bf5c61974738f4302f042c169b2ddb
Youssef97dev <youssefmoussafi97@gmail.com>
2025-09-04 11:30:33 +0100

    add text in farm house

--- src/components/Farm.jsx
+++ src/components/Farm.jsx
@@ -26,6 +26,38 @@ const Farm = () => {
         height={1000}
         className="object-cover w-full"
       />
+      <div className="flex flex-col justify-start items-center gap-0">
+        <div className="w-full flex justify-between items-center gap-10">
+          <h1 className="text-[18px] leading-[20px] whitespace-nowrap">{`Pure Farm House`}</h1>
+          <p className="text-[13px] leading-[15px] text-left">
+            {t("farm.text_2")}
+          </p>
+        </div>
+        <p className="text-[13px] leading-[15px] text-left">
+          {t("farm.text_3")}
+        </p>
+      </div>
+      <Image
+        src="/farm-1.webp"
+        alt="pure farm house"
+        width={1000}
+        height={1000}
+        className="object-cover w-full"
+      />
+      <Image
+        src="/farm-5.webp"
+        alt="pure farm house"
+        width={1000}
+        height={1000}
+        className="object-cover w-full"
+      />
+      <Image
+        src="/farm-2.webp"
+        alt="pure farm house"
+        width={1000}
+        height={1000}
+        className="object-cover w-full"
+      />
     </div>
   );
 };
